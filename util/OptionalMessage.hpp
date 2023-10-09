c50e73f669e442a7f99700c6f25d9b470dfe96b2
folosuru <folosuru@gmail.com>
2023-10-09 16:29:27 +0900

    OptionalMessage

--- util/OptionalMessage.hpp
+++ util/OptionalMessage.hpp
@@ -1,68 +1,53 @@
+#pragma once
+#ifndef TES_UTIL_OPTIONAL_MESSAGE_HPP_
+#define TES_UTIL_OPTIONAL_MESSAGE_HPP_
 #include <string>
 #include <stdexcept>
-template<typename T> class OptionalMessage {
+#include <utility>
+namespace tes::util {
+template<class T, class U> class OptionalMessage {
 public:
-    OptionalMessage(T value_) : value(value_), flag(true) {};
-    OptionalMessage(std::string message_): message(message_), flag(false){}
+    explicit OptionalMessage(T value_) : succeed_value(value_), flag(true) {};
+    explicit OptionalMessage(U message_): fail_value(std::move(message_)), flag(false){}
     explicit operator bool() const noexcept {
         return flag;
     }
-    T getValue() const {
+    [[nodiscard]]
+    T get_succeed() const {
         if (!flag) {
             throw std::runtime_error("");
         }
-        return value;
+        return succeed_value;
     }
-    std::string getMessage() const {
+    [[nodiscard]] U get_fail() const {
         if (flag) {
             throw std::runtime_error("");
         }
-        return message;
+        return fail_value;
     }
 private:
     bool flag;
-    T value;
-    std::string message;
+    T succeed_value;
+    U fail_value;
 };
-template<> class OptionalMessage<std::string> {
-public:
-    OptionalMessage(std::string object_, bool isValue)
-     : object(object_), flag(isValue) {}
-
-    explicit operator bool() const noexcept {
-        return flag;
-    }
-    std::string getValue() const {
-        if (!flag) {
-            throw std::runtime_error("");
-        }
-        return object;
-    }
-    std::string getMessage() const {
-        if (flag) {
-            throw std::runtime_error("");
-        }
-        return object;
-    }
-private:
-    bool flag;
-    std::string object;
-};
-template<> class OptionalMessage<void> {
+template<class U> class OptionalMessage<void,U> {
 public:
     OptionalMessage() : flag(true) {};
-    OptionalMessage(std::string message_) : message(message_), flag(false) {}
+    explicit OptionalMessage(U value) : fail_value(std::move(value)), flag(false) {}
 
     explicit operator bool() const noexcept {
         return flag;
     }
-    std::string getMessage() const {
+    [[nodiscard]]
+    U get_fail() const {
         if (flag) {
             throw std::runtime_error("");
         }
-        return message;
+        return fail_value;
     }
 private:
     bool flag;
-    std::string message;
+    U fail_value;
 };
+}
+#endif
