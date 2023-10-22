bfa0ee7343f4eafab05105fc79864e2dea1593e0
folosuru <folosuru@gmail.com>
2023-10-22 22:47:37 +0900

    lint

--- country/test/test.cpp
+++ country/test/test.cpp
@@ -10,12 +10,11 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 
 void Citizen_test() {
-    using namespace tes;
     cpptf::change_section("Citizen");
-    std::shared_ptr<CitizenRefer> refer = std::make_shared<CitizenRefer>();
-    std::shared_ptr<Country> country = std::make_shared<Country>("Japan",1);
+    std::shared_ptr<tes::CitizenRefer> refer = std::make_shared<tes::CitizenRefer>();
+    std::shared_ptr<tes::Country> country = std::make_shared<tes::Country>("Japan", 1);
     std::string_view perm = "foo";
-    auto citizen_ = Citizen(refer, country, "taro");
+    auto citizen_ = tes::Citizen(refer, country, "taro");
     cpptf::isFalse("permission not found", citizen_.hasPermission(perm));
     citizen_.addPermission(perm);
     cpptf::isTrue("permission found", citizen_.hasPermission(perm));
@@ -24,11 +23,9 @@ void Citizen_test() {
 }
 
 void load_citizen() {
-    using namespace tes;
     cpptf::change_section("Citizen");
-    std::shared_ptr<CitizenRefer> refer = std::make_shared<CitizenRefer>();
-    std::shared_ptr<Country> country = std::make_shared<Country>("Japan",1);
-
+    std::shared_ptr<tes::CitizenRefer> refer = std::make_shared<tes::CitizenRefer>();
+    std::shared_ptr<tes::Country> country = std::make_shared<tes::Country>("Japan", 1);
 }
 
 void permission_manager_test() {
@@ -39,37 +36,11 @@ void permission_manager_test() {
         std::string text1("Hoge");
         view = mng.getSv(text1);
     }
-    cpptf::isSame("hold scope",view,"Hoge");
+    cpptf::isSame("hold scope", view, "Hoge");
 }
-
-void sql_t() {
-    try
-    {
-        SQLite::Database    db("transaction.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
-
-        db.exec("DROP TABLE IF EXISTS test");
-
-        // Begin transaction
-        SQLite::Transaction transaction(db);
-
-        db.exec("CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)");
-
-        int nb = db.exec("INSERT INTO test VALUES (NULL, \"test\")");
-        std::cout << "INSERT INTO test VALUES (NULL, \"test\")\", returned " << nb << std::endl;
-
-        // Commit transaction
-        transaction.commit();
-    }
-    catch (std::exception& e)
-    {
-        std::cout << "exception: " << e.what() << std::endl;
-    }
-}
-
 
 int main() {
     permission_manager_test();
-    sql_t();
     Citizen_test();
     cpptf::complete();
     return 0;
