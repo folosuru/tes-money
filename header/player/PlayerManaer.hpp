3ecdbbd15b4e15c44c01239737d46f1bfbe10840
folosuru <folosuru@gmail.com>
2023-11-14 17:23:07 +0900

    (q_q)

---
+++ header/player/PlayerManaer.hpp
@@ -0,0 +1,20 @@
+#include <player/Player.hpp>
+namespace tes {
+class PlayerManager {
+public:
+    /*
+promise<Player> get() {
+if (this->players.contains(name)) {
+    return this->players.at(name);
+} else {
+    if (all_player_index.contains(name);) {
+        return // DBから読み出す
+    } else {
+        return // 新規
+    }
+}
+
+}
+    */
+};
+}
